9e5caa49a9ff613796aab490c08ff4c809f50576
Joaomarcellodev <joaomarcellobraz@gmail.com>
2024-11-16 18:44:38 -0300

    Código atualizado para ficar igual o projeto final

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
-const email = document.getElementById('login-email');
+const email = document.getElementById('email-login');
+const blurCadasterBtn = document.getElementById('blur-cadaster-btn');
+const blurLoginBtn = document.getElementById('blur-login-btn');
+const containerLogin = document.getElementById('container-left');
+const containerCadaster = document.getElementById('container-right');
+
+blurCadasterBtn.addEventListener('click', () => {
+    containerLogin.classList.remove('blur-container');
+    containerLogin.classList.add('onblur-container');    
+    containerCadaster.classList.remove('onblur-container');
+    containerCadaster.classList.add('blur-container');
+});
+
+blurLoginBtn.addEventListener('click', () => {
+    containerLogin.classList.remove('onblur-container');
+    containerLogin.classList.add('blur-container');
+    containerCadaster.classList.remove('blur-container');
+    containerCadaster.classList.add('onblur-container');
+});
 
 email.addEventListener('change', () => {
     if (!validarEmail(email.value)) {
         email.style.outline = '2px solid #ff6666';
+        email.classList.add('animated', 'shake');
     } else {
-        email.style.outline = '2px solid #66ff66';
+        email.style.outline = '2px solid transparent';
     }
 });
 
